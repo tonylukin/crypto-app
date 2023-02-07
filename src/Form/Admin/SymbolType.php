77b12c73e639fdaf95636b0bd8b9fa2e614479f7
Anton Lukin <mininzidane@gmail.com>
2023-02-08 02:20:07 +1000

    (fix) add error on existing symbol

--- src/Form/Admin/SymbolType.php
+++ src/Form/Admin/SymbolType.php
@@ -7,10 +7,12 @@ namespace App\Form\Admin;
 use App\Entity\Symbol;
 use App\Entity\UserSymbol;
 use App\Repository\SymbolRepository;
+use App\Repository\UserSymbolRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -19,6 +21,7 @@ class SymbolType extends AbstractType
 {
     public function __construct(
         private SymbolRepository $symbolRepository,
+        private UserSymbolRepository $userSymbolRepository,
     ) {
     }
 
@@ -60,6 +63,11 @@ class SymbolType extends AbstractType
         $symbolName = $event->getForm()->get('name')->getData();
 
         $symbol = $this->symbolRepository->findOneBy(['name' => $symbolName]);
+        if ($symbol !== null && $this->userSymbolRepository->findOneBySymbolAndUser($userSymbol->getUser(), $symbol) !== null) {
+            $event->getForm()->get('name')->addError(new FormError("Symbol '{$symbolName}' already exists for user {$userSymbol->getUser()->getUserIdentifier()}"));
+            return;
+        }
+
         if ($symbol === null) {
             $symbol = new Symbol();
             $symbol->setName($symbolName);
